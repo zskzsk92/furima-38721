0587fe1a851b45661f641a3c304947aa6843ffee
zskzsk92 <zsk1992on@gmail.com>
2022-12-23 21:57:24 +0900

    販売手数料計算、修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,5 @@
-function priceInput (){
-  const price = document.getElementById("item-price");
+function price (){
+  const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
