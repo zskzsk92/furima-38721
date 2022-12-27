b2c5ca6b58a61360393bf60e83e009d7e5cdf1b4
zskzsk92 <zsk1992on@gmail.com>
2022-12-27 14:15:25 +0900

    案内文の削除、リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!	,except: [ :index, :show]
   before_action :current_user_check, only: [:edit, :destroy, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-     @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to  @item
     else
@@ -47,6 +45,10 @@ class ItemsController < ApplicationController
     unless  Item.find(params[:id]).user.id  == current_user.id
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
