03fa3a18f7a3ab108cbd57393ab661722ac98c82
zskzsk92 <zsk1992on@gmail.com>
2022-12-30 19:02:50 +0900

    画面遷移の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    sold_out_item
   end
 
   def edit
-    sold_out_item
+    if @item.order.nil?
+    else
+      redirect_to root_path 
+    end
   end
 
   def update
@@ -57,13 +59,6 @@ end
 
   def set_item
     @item = Item.find(params[:id])
-  end
-
-  def sold_out_item
-    if @item.order.nil?
-    else
-      redirect_to root_path 
-    end
   end
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :item, only: [:index, :create]
+  before_action :non_purchased_item, only: [:index, :create]
   
   def index
     @order_shipping_address = OrderShippingAddress.new
-    
   end
 
   def create
@@ -35,6 +34,11 @@ private
 
   def item
     @item = Item.find(params[:item_id])
+  end
+
+  def non_purchased_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
 end
